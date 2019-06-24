092149222921d93ac110f016737cac3c8172ae85
StijnWijnen <stijnwijnen1997@gmail.com>
2019-06-24 17:22:51 +0200

    nette versie code en graph toegevoegd

--- results.py
+++ results.py
@@ -1,5 +1,6 @@
 import csv
 import nltk
+import matplotlib.pyplot as plt
 
 def average():
     total_val = 0
@@ -9,19 +10,26 @@ def average():
     with open('JEOPARDY.csv') as csvfile:
         data = csv.reader(csvfile, delimiter=',')
         for row in data:
+
+            # only questions in the round Jeopardy!
             if row[2] == 'Jeopardy!':
+
+                # get a value as integer corresponding with the diffuculty of the question
                 value = row[4][1:]
                 for char in value:
                     if char in ".,":
                         value = value.replace(char, '')
                 val = int(value)
+
+                # get the question
                 question = row[5]
 
+                # add all values to a total and keep track of counter
                 total_val += val
                 total_sentlen += len(nltk.word_tokenize(question))
                 counter += 1
 
-                #print(val, question)
+        # print average money value and average question length
         avg_val = total_val / counter
         avg_len = total_sentlen / counter
         print(avg_val, avg_len)
@@ -33,18 +41,32 @@ def result_data():
     easy_long = 0
     easy_short = 0
 
+    data_val = []
+    data_que = []
+
     with open('JEOPARDY.csv') as csvfile:
         data = csv.reader(csvfile, delimiter=',')
         for row in data:
+
+            # only questions in the round Jeopardy!
             if row[2] == 'Jeopardy!':
+
+                # get a value as integer corresponding with the difficulty of the question
                 value = row[4][1:]
                 for char in value:
                     if char in ".,":
                         value = value.replace(char, '')
                 val = int(value)
+
+                # get value for amount of tokens in a question
                 question = row[5]
                 que_len = len(nltk.word_tokenize(question))
 
+                # add datapoint for graph
+                data_val += [val]
+                data_que += [que_len]
+
+                # checks if question is long/short and difficult/easy
                 if val > 490:
                     if que_len > 16:
                         diff_long += 1
@@ -56,13 +78,30 @@ def result_data():
                     else:
                         easy_short += 1
 
+    #make a graph
+    makeplot(data_que, data_val)
+
+    #print values for the confusion matrix
     print("diff_long:", diff_long, "\ndiff_short:", diff_short, "\neasy_long:", easy_long, "\neasy_short:", easy_short)
 
 
 
+def makeplot(x, y):
+    # plotting the points
+    plt.scatter(x, y, label= "stars", color= "blue", marker= ".", s=30)
+    # naming the x axis
+    plt.xlabel('tokens in question')
+    # naming the y axis
+    plt.ylabel('money value of question')
+    # giving a title to my graph
+    plt.title('question difficulty vs question length')
+    # function to show the plot
+    plt.savefig("temp.png")
+    plt.show()
+
 
 def main():
-    #average()
+    average()
     result_data()
 
 
